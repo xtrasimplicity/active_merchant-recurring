3f285c4cce212604338db83586850780ce0e2f7a
Andrew Walter <andrew.walter@burnet.edu.au>
2018-05-25 15:17:43 +1000

    Initial commit

---
+++ lib/activemerchant/recurring.rb
@@ -0,0 +1,7 @@
+require "activemerchant/recurring/version"
+
+module Activemerchant
+  module Recurring
+    # Your code goes here...
+  end
+end
---
+++ lib/activemerchant/recurring/version.rb
@@ -0,0 +1,5 @@
+module Activemerchant
+  module Recurring
+    VERSION = "0.1.0"
+  end
+end
